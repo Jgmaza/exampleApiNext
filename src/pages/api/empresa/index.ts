81a657ed5b965c694d9b27d14fea4b77f515ae17
Jose Maza <111375069+jgmaza-infinity@users.noreply.github.com>
2024-05-01 14:20:35 -0500

    Refactor API handler in empresa/index.ts to return an array of mg001_empresas

--- src/pages/api/empresa/index.ts
+++ src/pages/api/empresa/index.ts
@@ -1,22 +1,20 @@
-import { Prisma, PrismaClient } from '@prisma/client'
+import { PrismaClient, mg001_empresas } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from 'next';
+import error from 'next/error';
 
-import type { mg001_empresas } from '@prisma/client'; // Import the Mg001Empresas type
+const prisma = new PrismaClient();
 
-const prisma = new PrismaClient()
-
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<mg001_empresas[]> // Change the response type to an array of mg001_empresas
+  res: NextApiResponse<mg001_empresas[] | error>
 ) {
-  if (req.method === "GET") {
-    return prisma.mg001_empresas.findMany().then((empresas) => {
+  try {
+    if (req.method === 'GET') {
+      const empresas = await prisma.mg001_empresas.findMany();
       return res.status(200).json(empresas);
-    }).catch((error: Prisma.PrismaClientKnownRequestError) => {
-      console.error(error);
-      return res.status(500).json([{ mg001_id: 0, mg001_razon_social: null, mg001_nit: null, mg001_estado: null, mg001_direccion: null, mg001_telefono: null, mg001_celular: null, mg001_pais: null }]);
-    });
+    }
+  } catch (error) {
+    console.log('Error: ', error);
+    return res.status(500).json(error as error);
   }
 }
